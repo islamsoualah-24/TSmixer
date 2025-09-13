32baa4da596b1f6a4889f53b4cd2d12419836a00
islamsoualah-24 <islamsoualah2017@gmail.com>
2025-09-13 11:43:00 +0100

    Update tsmixer.py

--- models/tsmixer.py
+++ models/tsmixer.py
@@ -15,20 +15,19 @@ def res_block(inputs, norm_type, activation, dropout, ff_dim):
   )
 
   # Temporal Linear
-  # x = norm(axis=[-2, -1])(inputs)
-
-  # بدل tf.transpose -> layers.Permute
   x = layers.Permute((2, 1))(inputs)  # [Batch, Channel, Input Length]
-  x = layers.Dense(tf.shape(inputs)[1], activation=activation)(x)
+
+  # هنا نستعمل inputs.shape[-1] (قيمة ثابتة) مش tf.shape
+  x = layers.Dense(inputs.shape[1], activation=activation)(x)
+
   x = layers.Permute((2, 1))(x)       # [Batch, Input Length, Channel]
   x = layers.Dropout(dropout)(x)
   res = x + inputs
 
   # Feature Linear
-  # x = norm(axis=[-2, -1])(res)
   x = layers.Dense(ff_dim, activation=activation)(res)
   x = layers.Dropout(dropout)(x)
-  x = layers.Dense(inputs.shape[-1])(x)
+  x = layers.Dense(inputs.shape[-1])(x)  # [Batch, Input Length, Channel]
   x = layers.Dropout(dropout)(x)
   return x + res
 
